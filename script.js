ebd910c5e7fbc7de5a9bc87e738d8ebd65c90709
Alexsander Leal <alexsanderunr@gmail.com>
2024-09-24 18:25:18 -0300

    função javascript adicionada, junto as imagens da "experiência soulfit"

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function aparecerMenu() {
         background_noclick.className = "background-noclick";
     }
 }
+
+function openModal(img) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const captionText = document.getElementById("caption");
+    
+    modal.style.display = "block";
+    modalImg.src = img.src; // Define a imagem do modal como a imagem clicada
+    captionText.innerHTML = img.alt; // Adiciona a descrição da imagem
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none"; // Oculta o modal
+}
