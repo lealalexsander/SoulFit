6108515464ee38a6f79abec089c60f41a7af11a2
Alexsander Leal <alexsanderunr@gmail.com>
2024-10-08 16:50:25 -0300

    responsividade das tabelas para mobile

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none"; // Oculta o modal
 }
+
+function mostrarMenos1() {
+    const descricao_tabela = document.getElementById("tabela-active_1");
+    let button_seta = document.getElementById("button-plano_1");
+
+    if(descricao_tabela.className === "tabela-active_1") {
+        descricao_tabela.className = "tabela-desactive_1";
+        button_seta.innerHTML = '<h3 class="button_texto">Mostrar mais</h3>       <img id="button_seta" class="button_seta" src="./src/icon/seta-para-baixo.png" alt="seta para baixo">'
+    } else {
+        descricao_tabela.className = "tabela-active_1";
+        button_seta.innerHTML = '<h3 class="button_texto">Mostrar menos</h3>       <img id="button_seta" class="button_seta" src="./src/icon/seta-para-cima.png" alt="seta para cima">'
+    }
+}
+
+function mostrarMenos2() {
+    const descricao_tabela = document.getElementById("tabela-active_2");
+    let button_seta = document.getElementById("button-plano_2");
+
+    if(descricao_tabela.className === "tabela-active_2") {
+        descricao_tabela.className = "tabela-desactive_2";
+        button_seta.innerHTML = '<h3 class="button_texto">Mostrar mais</h3>       <img id="button_seta" class="button_seta" src="./src/icon/seta-para-baixo.png" alt="seta para baixo">'
+    } else {
+        descricao_tabela.className = "tabela-active_2";
+        button_seta.innerHTML = '<h3 class="button_texto">Mostrar menos</h3>       <img id="button_seta" class="button_seta" src="./src/icon/seta-para-cima.png" alt="seta para cima">'
+    }
+}
+
+function mostrarMenos3() {
+    const descricao_tabela = document.getElementById("tabela-active_3");
+    let button_seta = document.getElementById("button-plano_3");
+
+    if(descricao_tabela.className === "tabela-active_3") {
+        descricao_tabela.className = "tabela-desactive_3";
+        button_seta.innerHTML = '<h3 class="button_texto">Mostrar mais</h3>       <img id="button_seta" class="button_seta" src="./src/icon/seta-para-baixo.png" alt="seta para baixo">'
+    } else {
+        descricao_tabela.className = "tabela-active_3";
+        button_seta.innerHTML = '<h3 class="button_texto">Mostrar menos</h3>       <img id="button_seta" class="button_seta" src="./src/icon/seta-para-cima.png" alt="seta para cima">'
+    }
+}
